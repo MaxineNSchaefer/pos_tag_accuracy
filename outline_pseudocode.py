aa4cff36bab1d6066e0bb988969f1d2dd956d955
MaxineNSchaefer <mndiemer@gmail.com>
2023-03-30 15:00:14 -0400

    upload pseudocode

---
+++ outline_pseudocode.py
@@ -0,0 +1,10 @@
+
+# use 80% of conllu file to train tagger
+
+# keep 20% of conllu file for testing
+
+# run the test data through our pos tagger, save the tags
+
+# compare the actual and predicted words
+
+# calculate accuracy
