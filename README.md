98d0eed8717d847c632fccf0444597e7bd700eb5
MaxineNSchaefer <mndiemer@gmail.com>
2023-03-30 15:09:18 -0400

    update readme and refine pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pos_tag_accuracy
+
+This repo includes code for determing the accuracy of a part of speecg tagger. This work is based on group work (see list of contributors).
--- outline_pseudocode.py
+++ outline_pseudocode.py
@@ -1,10 +1,14 @@
 
-# use 80% of conllu file to train tagger
+# data from conllu file object
+## randomly select 80% of data for training,
+## randomly select the other 20% for testing
 
-# keep 20% of conllu file for testing
+## save the original tags from the test dataset
 
-# run the test data through our pos tagger, save the tags
+# pos tagger
+## create a pos tagger using the training data
 
-# compare the actual and predicted words
+## run the test data through our pos tagger, save the new tags
 
-# calculate accuracy
+# calculate accuracy - compare the actual (original) and predicted (new) tags
+## see compute_accuracy function in this post for ideas: https://necromuralist.github.io/Neurotic-Networking/posts/nlp/pos-tagging-accuracy-of-model/
